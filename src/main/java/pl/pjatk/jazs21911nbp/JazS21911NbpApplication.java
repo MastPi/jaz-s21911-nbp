33f9278bcbad3d2e82583116f1408fa70743fe13
Piotr <s21911@pjwstk.edu.pl>
2021-07-03 13:43:02 +0200

    starting exam and resttemplate added

---
+++ src/main/java/pl/pjatk/jazs21911nbp/JazS21911NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs21911nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS21911NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS21911NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pjatk/jazs21911nbp/JazS21911NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs21911nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS21911NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
